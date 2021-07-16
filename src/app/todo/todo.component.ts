234cb91f205a1d15753fd74c40ecc925681b84e4
aminurizam <aminurizam@gmail.com>
2021-07-16 14:23:30 +0800

    update

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -14,42 +14,18 @@ export class TodoComponent implements OnInit {
 	// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-7.html#definite-assignment-assertions
 	// refs: https://stackoverflow.com/questions/49699067/property-has-no-initializer-and-is-not-definitely-assigned-in-the-construc/60113028#60113028
 	todos!: Todo[];
-
-	// todo: string = '';
-	todo = new FormControl('', [Validators.required]);
+	todo = new FormControl('');
 	percentageCompleted: number = 0;
 	showPercentage: boolean = false;
-	// title: string = '';
-	title = new FormControl('', Validators.required);
 
 	ngOnInit(): void {
-		this.todos = [
-			{
-				title: 'sudah makan belum',
-				completed: true,
-			},
-			{
-				title: 'sudah minum belum',
-				completed: true,
-			},
-			{
-				title: 'sudah kahwin belum sudah kahwin belum  sudah kahwin belum sudah kahwin belum  sudah kahwin belum ',
-				completed: false,
-			},
-		];
+		this.todos = [];
 
 		this.calculatePercentageCompleted();
 
 	}
 
-	get testTitle() { return this.title }
-
-
 	calculatePercentageCompleted() {
-		// count percentage of task done. eg: if 1 task done, percentage should be 33.33%
-		// loop through todos object and count item completed true
-		// total item complete / total item * 100%
-		// assign to variable
 		this.showPercentage = true;
 		let todoCompleted = 0;
 		this.todos.forEach((value, key) => {
@@ -59,18 +35,11 @@ export class TodoComponent implements OnInit {
 			(todoCompleted / this.todos.length) * 100
 		);
 		if(isNaN(this.percentageCompleted)) this.showPercentage = false;
-		// console.log(this.percentageCompleted);
-
-	}
-
-	log(todo:any) {
-		console.log(todo);
 	}
 
 	addTodo() {
 		// not sure how to do assign as Object on this
 		let uniqueTodo = (value:any) => value.title !== this.todo.value
-		console.log(this.todo);
 
 		if(!this.todo.value) return this.todo.setErrors({ required: true });
 
